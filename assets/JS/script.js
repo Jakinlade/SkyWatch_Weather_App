ede296caea10158c3127c838639f3c145622f81d
Jakinlade <jj.47.johnson@gmail.com>
2023-02-25 14:06:39 +0000

    properly linked css and js files to html

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,19 +1,19 @@
-//Api key for openweather
+// My Api key for openweather
 
 // function to get current weather for the city i choose
 
-// handle successful response
+    // handle successful response
 
-// update current weather section of HTML
+    // update current weather section of HTML
 
-// call function to get 5-day forecast for the same city
+    // call function to get 5-day forecast for the same city
 
 // function to get 5-day forecast for a city
 
-// handle successful response
-
-// add forecast to HTML
-
-// handle error
+    // handle successful response
+      
+      // add forecast to HTML
+   
+    // handle error
 
 // function to handle form submit
