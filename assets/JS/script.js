ce063bcf2a62baa083491a0525597e150d377bfe
Jakinlade <jj.47.johnson@gmail.com>
2023-02-25 14:08:27 +0000

    added API key and started ajax call function

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,7 +1,15 @@
 // My Api key for openweather
+const apiKey = "1809f1f474be4472f90bfc7c78003195"
 
 // function to get current weather for the city i choose
-
+function getCurrentWeather(city) {
+  const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  
+  $.ajax({
+    url: queryURL,
+    method: 'GET'
+  }).then(function(response) {});
+};
     // handle successful response
 
     // update current weather section of HTML
