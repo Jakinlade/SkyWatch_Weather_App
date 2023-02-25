6463eedaafd13ff78cdf2e87171da6854e154126
Jakinlade <jj.47.johnson@gmail.com>
2023-02-25 14:00:47 +0000

    wrote psuedo code js file

---
+++ assets/JS/script.js
@@ -0,0 +1,19 @@
+//Api key for openweather
+
+// function to get current weather for the city i choose
+
+// handle successful response
+
+// update current weather section of HTML
+
+// call function to get 5-day forecast for the same city
+
+// function to get 5-day forecast for a city
+
+// handle successful response
+
+// add forecast to HTML
+
+// handle error
+
+// function to handle form submit
